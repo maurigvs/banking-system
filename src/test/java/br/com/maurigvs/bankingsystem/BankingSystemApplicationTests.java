0330a1f68ade4f4ddc66ae191da7f36ba52d14b4
Mauri Gonçalves <mauri.goncalves@accountfy.com>
2023-06-01 08:47:38 -0300

    initial commit

---
+++ src/test/java/br/com/maurigvs/bankingsystem/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maurigvs.bankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
